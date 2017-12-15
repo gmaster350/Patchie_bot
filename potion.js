ade325043c84049628b1fd00eb26805b2596f20a
Glenn Whiting <glenn.a.whiting@gmail.com>
2017-12-15 15:49:48 +1000

    possible potions fix

--- potion.js
+++ potion.js
@@ -4,11 +4,13 @@ var effects = [
 "You feel many times stronger.",
 "You feel much more agile.",
 ["penis","head","tongue","legs","tail","ears","wings","whole body","ears","snout"],
-"You are able to see greater distances.",[" rabbit"," human"," wolf"," fish"," bear"," fox"," dragon"," cat"," dog"," mouse"," rat"," pig"," sheep"," giraffe"," zebra"," horse"," hippopotamus"," bird","n eagle"," shark"," whale"," sloth"," chicken"],
+"You are able to see greater distances.",
+[" rabbit"," human"," wolf"," fish"," bear"," fox"," dragon"," cat"," dog"," mouse"," rat"," pig"," sheep"," giraffe"," zebra"," horse"," hippopotamus"," bird","n eagle"," shark"," whale"," sloth"," chicken"],
 ["red","orange","yellow","green","blue","purple","pink","black","white","grey","transparent","stripey","spotted"],
 "Your skin is immune to any acids",
 "Your tongue turns numb, leaving you unable to speak coherently",
-"You have a compulsion to dance",["tea","coffee","water","cola soda","deodorant","molten nickel","magma","candy","rosemary and thyme","paprika","parsley","mcdonald's fries","old sneakers","dulux paint","sand","gravel","salt","small plastic toys","powerful pheromones","sleeping gas","shredded paper","propane","pennies"]];
+"You have a compulsion to dance",
+["tea","coffee","water","cola soda","deodorant","molten nickel","magma","candy","rosemary and thyme","paprika","parsley","mcdonald's fries","old sneakers","dulux paint","sand","gravel","salt","small plastic toys","powerful pheromones","sleeping gas","shredded paper","propane","pennies"],"You become invisible to others"];
 
 function rn(n){ //random number between 0 and n
 	return Math.floor(Math.random()*n);
@@ -17,18 +19,21 @@ function rn(n){ //random number between 0 and n
 function generate(message,callback){
 	var index = rn(effects.length);
 	var response;
+	var jindex = effects[index] instanceof Array ? rn(effects[index].length) : -1);
+	
 	switch(index){
 		case 2:
-			response = "Your" + effects[index][rn(effects[index].length)] + " expands to " + String(rn(8)+2) + " times "+rn(2)?"larger":"smaller"+" than its current size.";
+			response = "Your" + effects[index][jindex] + " expands to " + String(rn(8)+2) + " times "+rn(2)?"larger":"smaller"+" than its current size.";
 			break;
 		case 4:
-			response = "You become invisible to others","Your body gradually transforms into that of a" + effects[index][rn(effects[index].length)];
+			response = "Your body gradually transforms into that of a" + effects[index][jindex];
 			break;
 		case 5:
-			response = "Your skin starts to change color, gradually turning " + effects[index][rn(effects[index].length)] + ".";
+			response = "Your skin starts to change color, gradually turning " + effects[index][jindex] + ".";
 			break;
 		case 9:
-			response = "You gain the ability to exhale " + effects[index][rn(effects[index].length)];
+			response = "You gain the ability to exhale " + effects[index][jindex].length)];
+			break;
 		default:
 			response = effects[index];
 			break;
