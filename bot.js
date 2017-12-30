aded7f3169b1950258d1a242d79792a3de807a3b
Glenn Whiting <glenn.a.whiting@gmail.com>
2017-12-30 17:29:09 +1000

    added apostrophes as a 'clean' character type

--- bot.js
+++ bot.js
@@ -530,7 +530,7 @@ Array.prototype.hasEach = function(list,insensitive){
 //remove non-alphanumeric characters
 String.prototype.clean = function(){
 	var ret = "";
-	var cleanChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"," "];
+	var cleanChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"," ","'"];
 	for(var i = 0; i < this.length; i++){
 		if(cleanChars.some(function(c){
 			return this.charAt(i) == c;
